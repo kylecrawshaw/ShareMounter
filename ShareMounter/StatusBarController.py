f0dcd36c05e38d6b54261d1602a5dc717f96fcb6
Kyle Crawshaw <kyle.crawshaw@gmail.com>
2015-11-20 12:41:14 -0500

    Disabled SSO checkbox (for now)

--- ShareMounter/StatusBarController.py
+++ ShareMounter/StatusBarController.py
@@ -13,11 +13,14 @@ from SystemConfiguration import *
 import objc
 import os
 import SMUtilities
+from pymacad import *
+import PyDialog
 import CoreFoundation
 import subprocess
 
 
 class StatusBarController(NSObject):
+    '''Main controller object for UI features'''
     mainMenu = None
     connectMenu = None
     statusBar = None
@@ -34,8 +37,8 @@ class StatusBarController(NSObject):
     addNewButton = objc.IBOutlet()
     removeButton = objc.IBOutlet()
     doneButton = objc.IBOutlet()
-    useSSOCheckbox = objc.IBOutlet()
-    loginButton = objc.IBOutlet()
+    # useSSOCheckbox = objc.IBOutlet()
+    # loginButton = objc.IBOutlet()
 
 
     passwordPanel = objc.IBOutlet()
@@ -50,25 +53,27 @@ class StatusBarController(NSObject):
 
     menu_is_updating = False
 
-    user = SMUtilities.DirectoryUser()
-    kerberos = SMUtilities.Kerberos(user)
+#    user = SMUtilities.DirectoryUser()
+    # kerberos = SMUtilities.Kerberos(user)
     config_manager = SMUtilities.ConfigManager()
 
 
     def runStartup(self):
-        # self.ldap_reachable = SMUtilities.is_ldap_reachable(self.user.get_addomain())
-        # if self.ldap_reachable:
-        #     Utilities.kerberos_valid()
-        #     self.config_manager.update_managedshares(self.user)
         self.statusBar = NSStatusBar.systemStatusBar().statusItemWithLength_(-1.0)
         statusBarImage = NSImage.imageNamed_('DefaultStatusBarIcon')
         statusBarImage.setTemplate_(True)
         self.statusBar.button().setImage_(statusBarImage)
         self.buildMainMenu()
         self.updateConfig()
-        # self.buildConnectMenu()
         self.registerForWorkspaceNotifications()
         self.detect_network_changes()
+
+    def updateConfig(self):
+        self.config_manager.validate_kerberos()
+        self.ldap_reachable = ad.accessible(self.config_manager.domain)
+        if self.ldap_reachable:
+            self.config_manager.update_managedshares(self.config_manager.principal)
+        self.buildConnectMenu()
 
     @objc.IBAction
     def quit_(self, sender):
@@ -81,6 +86,7 @@ class StatusBarController(NSObject):
         self.mainMenu.addItemWithTitle_action_keyEquivalent_('Loading...', None, '').setTarget_(self)
         self.mainMenu.addItem_(NSMenuItem.separatorItem())
         self.mainMenu.addItemWithTitle_action_keyEquivalent_('Manage Network Shares', self.manageNetworkShares_, '').setTarget_(self)
+        self.mainMenu.addItemWithTitle_action_keyEquivalent_('Ticket Viewer', self.openTicketViewer_, '').setTarget_(self)
         self.mainMenu.addItem_(NSMenuItem.separatorItem())
         self.mainMenu.addItemWithTitle_action_keyEquivalent_('Show Shares on Desktop', self.toggleShowDrivesOnDesktop_, '').setTarget_(self)
         if CoreFoundation.CFPreferencesCopyAppValue('ShowMountedServersOnDesktop', 'com.apple.finder'):
@@ -92,12 +98,15 @@ class StatusBarController(NSObject):
         self.mainMenu.addItemWithTitle_action_keyEquivalent_('Quit', self.quit_, '').setTarget_(self)
         self.statusBar.setMenu_(self.mainMenu)
 
+    @objc.IBAction
+    def openTicketViewer_(self, sender):
+        app_path = '/System/Library/CoreServices/Ticket Viewer.app'
+        NSWorkspace.sharedWorkspace().launchApplication_('Ticket Viewer')
+
     def releaseStatusBar(self):
         self.mainMenu.release()
-        # menu = nil;
         NSStatusBar.systemStatusBar().removeStatusItem_(self.statusBar)
         self.statusBar.release()
-        # self.statusBar = None
 
     def buildShareMenu(self, share):
         # NSLog('Building menu for "{0}"'.format(share.get('title')))
@@ -200,7 +209,6 @@ class StatusBarController(NSObject):
             if shareMenu.submenu() and hide_all:
                 shareMenu.setHidden_(True)
 
-        # if len(self.connectMenu.itemArray()) > 3:
         self.connectMenu.addItem_(NSMenuItem.separatorItem())
         if SMUtilities.get_mounted_network_volumes():
             self.connectMenu.addItemWithTitle_action_keyEquivalent_('Unmount All', self.unmountShare_, '').setTarget_(self)
@@ -255,20 +263,16 @@ class StatusBarController(NSObject):
     def connectToShare_(self, sender):
         NSLog('User clicked {}'.format(sender.parentItem().title()))
         network_share = self.config_manager.get_sharebykey('title', sender.parentItem().title())
-        if network_share.get('share_type') == 'user' and network_share.get('use_kerberos'):
-            if network_share.get('username') == None:
-                self.openPasswordPanel_(self)
-            else:
-                self.config_manager.check_keychain(network_share.get('share_url'),
-                                                   network_share.get('username'))
         SMUtilities.mount_share(network_share.get('share_url'),
                                 self.config_manager.user_config.get('display_notifications'))
 
     def getAvailableShares(self):
         available_shares = list()
+        print self.config_manager.managed_shares
         for share in self.config_manager.managed_shares:
             share['share_type'] = 'managed'
             available_shares.append(share)
+        print 'HAS NOT FAILED'
         for share in self.config_manager.user_added_shares:
             share['share_type'] = 'user'
             available_shares.append(share)
@@ -279,48 +283,6 @@ class StatusBarController(NSObject):
         print 'Close panel clicked'
         self.passwordPanel.orderOut_(self)
 
-    @objc.IBAction
-    def savePassword_(self, sender):
-        share_url = self.shareURLField.stringValue()
-        username = self.usernameFieldLogin.stringValue()
-        password = self.passwordFieldLogin.stringValue()
-
-        if self.config_manager.check_keychain(share_url, username):
-            self.config_manager.delete_share_from_keychain(share_url, username)
-        self.config_manager.add_share_to_keychain(share_url,
-                                                  username,
-                                                  password)
-
-        network_share, index = self.config_manager.get_useradded_bykey('share_url',
-                                                                self.shareURLField.stringValue())
-
-        self.config_manager.user_added_shares[index]['username'] = username
-        self.closePasswordPanel_(self)
-        self.usernameFieldLogin.setStringValue_(''),
-        self.passwordFieldLogin.setStringValue_('')
-        self.config_manager.save_prefs()
-
-
-    @objc.IBAction
-    def openPasswordPanel_(self, sender):
-        if self.shareURLField.stringValue() == '':
-            d = SMUtilities.PyDialog.AlertDialog('Missing fields!',
-                                                 'You must enter a url to the network share!')
-            d.display()
-        else:
-            self.saveButtonClicked_(self)
-            network_share, index = self.config_manager.get_useradded_bykey('share_url',
-                                                                    self.shareURLField.stringValue())
-            if network_share.get('username'):
-                if (self.config_manager.check_keychain(network_share.get('share_url'),
-                                                       network_share.get('username'))):
-                    username = network_share.get('username')
-                else:
-                    username = ''
-                self.usernameFieldLogin.setStringValue_(username)
-            self.serverNameLabel.setStringValue_(network_share.get('share_url'))
-            NSApplication.sharedApplication().activateIgnoringOtherApps_(objc.YES)
-            self.passwordPanel.makeKeyAndOrderFront_(self)
 
     @objc.IBAction
     def manageNetworkShares_(self, sender):
@@ -338,16 +300,17 @@ class StatusBarController(NSObject):
         self.networkSharesDropdown.addItemsWithTitles_(network_share_titles)
         self.setupManageShareWindow()
 
+
     @objc.IBAction
     def changeVisibleShare_(self, sender):
         self.setupManageShareWindow()
-
-    @objc.IBAction
-    def useSSOToggled_(self, sender):
-        if self.useSSOCheckbox.state():
-            self.loginButton.setHidden_(True)
-        else:
-            self.loginButton.setHidden_(False)
+    #
+    # @objc.IBAction
+    # def useSSOToggled_(self, sender):
+    #     if self.useSSOCheckbox.state():
+    #         self.loginButton.setHidden_(True)
+    #     else:
+    #         self.loginButton.setHidden_(False)
 
     @objc.IBAction
     def addNewShareClicked_(self, sender):
@@ -355,14 +318,14 @@ class StatusBarController(NSObject):
         self.shareTitleField.setEnabled_(True)
         self.shareURLField.setStringValue_('')
         self.shareURLField.setEnabled_(True)
-        self.useSSOCheckbox.setEnabled_(True)
-        self.useSSOCheckbox.setState_(False)
+        # self.useSSOCheckbox.setEnabled_(True)
+        # self.useSSOCheckbox.setState_(False)
         self.hideFromMenuCheck.setState_(False)
         self.connectAutoCheck.setState_(False)
         self.networkSharesDropdown.addItemsWithTitles_(['New Share'])
         self.networkSharesDropdown.selectItemWithTitle_('New Share')
         self.removeButton.setHidden_(False)
-        self.loginButton.setHidden_(False)
+        # self.loginButton.setHidden_(False)
 
     @objc.IBAction
     def saveButtonClicked_(self, sender):
@@ -371,7 +334,7 @@ class StatusBarController(NSObject):
         auto_connect = True if self.connectAutoCheck.state() else False
         hide_from_menu = True if self.hideFromMenuCheck.state() else False
         selected_item = self.networkSharesDropdown.selectedItem()
-        use_kerberos = True if self.useSSOCheckbox.state() else False
+        # use_kerberos = True if self.useSSOCheckbox.state() else False
 
         if share_title != selected_item.title():
             index = self.networkSharesDropdown.indexOfSelectedItem()
@@ -392,7 +355,7 @@ class StatusBarController(NSObject):
                 self.config_manager.user_added_shares[existing_index]['hide_from_menu'] = hide_from_menu
                 self.config_manager.user_added_shares[existing_index]['title'] = share_title
                 self.config_manager.user_added_shares[existing_index]['share_url'] = share_url
-                self.config_manager.user_added_shares[existing_index]['use_kerberos'] = use_kerberos
+                # self.config_manager.user_added_shares[existing_index]['use_kerberos'] = use_kerberos
             connect_menu_item = self.connectMenu.itemWithTitle_(selected_item.title())
             connect_menu_item.setTitle_(share_title)
             if self.ldap_reachable and not self.connectMenu.itemWithTitle_('Show Hidden').state():
@@ -444,21 +407,21 @@ class StatusBarController(NSObject):
             self.shareTitleField.setEnabled_(False)
             self.shareURLField.setEnabled_(False)
             self.removeButton.setHidden_(True)
-            self.loginButton.setHidden_(True)
-            self.useSSOCheckbox.setEnabled_(False)
-            self.useSSOCheckbox.setState_(True)
-            self.loginButton.setHidden_(True)
+            # self.loginButton.setHidden_(True)
+            # self.useSSOCheckbox.setEnabled_(False)
+            # self.useSSOCheckbox.setState_(True)
+            # self.loginButton.setHidden_(True)
         else:
             self.shareTitleField.setEnabled_(True)
             self.shareURLField.setEnabled_(True)
             self.removeButton.setHidden_(False)
-            self.useSSOCheckbox.setEnabled_(True)
-            if selected_share.get('use_kerberos') == 1:
-                self.useSSOCheckbox.setState_(True)
-                self.loginButton.setHidden_(True)
-            else:
-                self.useSSOCheckbox.setState_(False)
-                self.loginButton.setHidden_(False)
+            # self.useSSOCheckbox.setEnabled_(True)
+            # if selected_share.get('use_kerberos') == 1:
+            #     self.useSSOCheckbox.setState_(True)
+            #     self.loginButton.setHidden_(True)
+            # else:
+            #     self.useSSOCheckbox.setState_(False)
+            #     self.loginButton.setHidden_(False)
 
         self.hideFromMenuCheck.setState_(selected_share.get('hide_from_menu'))
         self.connectAutoCheck.setState_(selected_share.get('connect_automatically'))
@@ -466,7 +429,6 @@ class StatusBarController(NSObject):
 
     @objc.IBAction
     def toggleAutoConnect_(self, sender):
-
         share = self.config_manager.get_sharebykey('title', sender.parentItem().title())
         if share:
             if share.get('connect_automatically'):
@@ -620,16 +582,9 @@ class StatusBarController(NSObject):
             else:
                 NSLog('Connect Menu is empty. No need to update')
 
-    def updateConfig(self):
-        self.ldap_reachable = SMUtilities.is_ldap_reachable(self.user.get_addomain())
-        if self.ldap_reachable:
-            self.kerberos.kerberos_valid()
-            self.config_manager.update_managedshares(self.user)
-        self.buildConnectMenu()
-
     def networkStateHasChanged(self, store, keys, info):
         NSLog('Network state has changed')
-        self.ldap_reachable = SMUtilities.is_ldap_reachable(self.user.get_addomain())
+        self.ldap_reachable = SMUtilities.is_ldap_reachable(self.config_manager.domain)
         print 'Menu is updating: {0}'.format(self.menu_is_updating)
         if self.menu_is_updating == False:
             self.menu_is_updating = True
